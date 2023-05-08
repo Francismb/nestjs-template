862d519be050746eddb8fd77621f3cf49da70108
Francis Malloch-Boe <francismb@users.noreply.github.com>
2023-05-08 16:48:45 +1000

    Wrong port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const arr = [];
-  while (true) {
-    arr.push(new Object());
-  }
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  await app.listen(5000);
 }
 bootstrap();
