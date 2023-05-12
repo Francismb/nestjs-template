e8bf566aec56cc8e068ced021218782a447801bd
Francis Malloch-Boe <francismb@users.noreply.github.com>
2023-05-12 13:59:50 +1000

    Application Error

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,6 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, Logger],
+  providers: [AppService],
 })
 export class AppModule {}
